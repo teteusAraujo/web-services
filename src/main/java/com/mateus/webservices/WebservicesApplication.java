6b16dbfa129e929806dd7b680d3f2d7656c54f3a
Mateus Araújo <mateusaraujo996@gmail.com>
2020-09-08 13:03:08 -0300

    Criação da API rest de Web Services com Spring Boot

---
+++ src/main/java/com/mateus/webservices/WebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mateus/webservices/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateus.webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
